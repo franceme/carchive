750702fec2bf6bc94459442b1dcbeb82868e005e
franceme <frantzme@vt.edu>
2022-09-08 13:55:55 +0000

    updated devcontainer

--- .devcontainer/devcontainer.py
+++ .devcontainer/devcontainer.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 import os,sys
-run = lambda x:os.system(x)
 
-for x in ['Gruntfuggly.todo-tree', 'ms-python.python', 'actboy168.tasks', 'dchanco.vsc-invoke', 'donjayamanne.githistory', 'alefragnani.Bookmarks', 'littlefoxteam.vscode-python-test-adapter', 'njpwerner.autodocstring', 'sourcery.sourcery', 'GitHub.copilot', 'hbenl.vscode-test-explorer'
-					"Alpha4.jsonl",
-					"WakaTime.vscode-wakatime","cschleiden.vscode-github-actions"]:
-	run(f"code-server --install-extension {x}")
+try:
+	import funbelts as ut
+except:
+	os.system(f"{sys.executable} -m pip install --upgrade funbelts")
+	import funbelts as ut
+
+def run(package):
+	cmd = f"code-server --install-extension {package}"
+	print(cmd)
+	try:
+		os.system(cmd)
+	except Exception as e:
+		print(e)
+
+devcontainer = os.path.join(os.path.dirname(__file__), 'devcontainer.json')
+for extension in ut.cmt_json(devcontainer)['customizations']['vscode']['extensions']:
+	run(f"{sys.executable} -m pip install {extension}")
