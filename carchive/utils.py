e1adbf0e3fadca9e0dfeee9d774ff5ac4a9046fe
franceme <9311496+franceme@users.noreply.github.com>
2023-05-01 12:34:09 -0400

    Update

---
+++ carchive/utils.py
@@ -0,0 +1,20 @@
+def live_link(url: str):
+    import contextlib, requests, time
+    response = False
+    with contextlib.suppress(Exception):
+        response_type = requests.get(url)
+        response = response_type.status_code < 400
+        time.sleep(2)
+    return response
+
+def str_to_base64(string, encoding:str='utf-8'):
+    import base64
+    try:
+        return base64.b64encode(string.encode(encoding)).decode(encoding)
+    except Exception as e:
+        print(e)
+        return None
+
+def base64_to_str(b64, encoding:str='utf-8'):
+    import base64
+    return base64.b64decode(b64).decode(encoding)
